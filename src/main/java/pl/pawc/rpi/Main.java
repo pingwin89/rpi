cabadd48da1aedcc68df3a7290d5ffe24575a29a
pawc <pswid89@gmail.com>
2017-04-20 20:01:01 +0200

    pwm

--- src/main/java/pl/pawc/rpi/Main.java
+++ src/main/java/pl/pawc/rpi/Main.java
@@ -2,35 +2,33 @@ package pl.pawc.rpi;
 
 import java.util.Scanner;
 
-import com.pi4j.io.gpio.GpioController;
+/*import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.RaspiPin;*/
+
+import com.pi4j.wiringpi.SoftPwm;
 
 public class Main{
 
     public static void main(String[] args) throws InterruptedException {
 
-        final GpioController gpio = GpioFactory.getInstance();
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
-        pin.setShutdownOptions(true, PinState.LOW);
+        //final GpioController gpio = GpioFactory.getInstance();
+        //final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+        //pin.setShutdownOptions(true, PinState.LOW);
+        System.out.println("Create softPWM on pin 1");
+        SoftPwm.softPwmCreate(1, 0, 100);
 
         Scanner sc = new Scanner(System.in);
-        String input = "";
-        while(!"quit".equals(input)){
-        	input = sc.nextLine();
-        	if("on".equals(input)) {
-        		pin.high();
-        		System.out.println("turning on...");
-        	}
-        	if("off".equals(input)){
-        		pin.low();
-        		System.out.println("turning off...");
-        	}
+        int input = 0;
+        while(input!=-1){
+        	SoftPwm.softPwmWrite(1, input);
+        	System.out.println("Set value "+input);
         }
         System.out.println("Exiting...");
         sc.close();
-        gpio.shutdown();
+        SoftPwm.softPwmStop(1);
+        //gpio.shutdown();
     }
 }
